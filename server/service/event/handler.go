3ba4f9cd4ec1868f94e9187182fb1558577ff3e4
Capucinoxx <bendertermebc@gmail.com>
2022-03-31 19:23:13 -0400

    [feat] - clean

--- server/service/event/handler.go
+++ server/service/event/handler.go
@@ -2,7 +2,6 @@ package event
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/Capucinoxx/vibrance/server/model"
@@ -44,7 +43,6 @@ func (h handler) listEvent() http.HandlerFunc {
 			return
 		}
 
-		fmt.Println(events)
 		response.Write(w, http.StatusOK, response.Response{Data: events})
 	}
 }
