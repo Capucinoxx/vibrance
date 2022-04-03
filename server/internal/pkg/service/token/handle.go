916ffb9246d256c0a1997cbe04947edbfb6d06ff
Capucinoxx <bendertermebc@gmail.com>
2022-04-03 04:03:36 -0400

    [feat] - back liste routes oauth token

---
+++ server/internal/pkg/service/token/handle.go
@@ -0,0 +1,52 @@
+package token
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Capucinoxx/vibrance/internal/pkg/common/router"
+	"github.com/gocql/gocql"
+)
+
+type handler struct {
+	repo Repository
+}
+
+func Handle(conn *gocql.Session, timeout time.Duration) router.Router {
+	h := handler{repo: NewRepository(conn, timeout)}
+
+	routes := router.Routes{
+		{Method: http.MethodPost, Pattern: "/token_create", HandlerFunc: h.create()},
+		{Method: http.MethodPost, Pattern: "/token_refresh", HandlerFunc: h.refresh()},
+		{Method: http.MethodPost, Pattern: "/token_revoke", HandlerFunc: h.revoke()},
+		{Method: http.MethodPost, Pattern: "/token_find", HandlerFunc: h.find()},
+	}
+
+	r := router.Consumer("/oauth", routes, nil)
+
+	return r
+}
+
+func (h handler) create() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h handler) refresh() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h handler) revoke() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h handler) find() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
